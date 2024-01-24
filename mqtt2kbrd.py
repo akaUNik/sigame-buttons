8aafe01b2fdf58373455d5df0bc593ebb1a29607
Dmitry Burdakov <akaunik@gmail.com>
2024-01-24 22:00:55 +0300

    Home Assistant fix

--- mqtt2kbrd.py
+++ mqtt2kbrd.py
@@ -1,37 +1,40 @@
 import os
-import keyboard
 import json
 from paho.mqtt import client as mqtt_client
 
 
-broker = '192.168.1.14'
+broker = 'homeassistant.local'
 port = 1883
 topics = [
-    ('zigbee2mqtt/SwitchD1', 0),
-    ('zigbee2mqtt/SwitchD2', 0),
+    ('zigbee2mqtt/Button1/action', 0),
+    ('zigbee2mqtt/Button2/action', 0),
+    ('zigbee2mqtt/Button3/action', 0),
 ]
 client_id = 'sigame-buttons'
-# username = 'emqx'
-# password = 'public'
+username = 'mosquitto'
+password = 'public'
 
-def notify(message,title=None,subtitle=None,soundname=None):
+def notify(message, title=None, subtitle=None, soundname=None):
 	"""
 		Display an OSX notification with message title an subtitle
 		sounds are located in /System/Library/Sounds or ~/Library/Sounds
 	"""
 	titlePart = ''
 	if(not title is None):
-		titlePart = 'with title "{0}"'.format(title)
+		titlePart = f'with title "{title}"'
 	subtitlePart = ''
 	if(not subtitle is None):
-		subtitlePart = 'subtitle "{0}"'.format(subtitle)
+		subtitlePart = f'subtitle "{subtitle}"'
 	soundnamePart = ''
 	if(not soundname is None):
-		soundnamePart = 'sound name "{0}"'.format(soundname)
+		soundnamePart = f'sound name "{soundname}"'
 
-	appleScriptNotification = 'display notification "{0}" {1} {2} {3}'.format(message,titlePart,subtitlePart,soundnamePart)
-	os.system("osascript -e '{0}'".format(appleScriptNotification))
+	appleScriptNotification = f'display notification "{message}" {titlePart} {subtitlePart} {soundnamePart}'
+	os.system(f"osascript -e '{appleScriptNotification}'")
 
+def press(key):
+    tellAapplication = f'tell application "System Events" to keystroke "{key}"'
+    os.system(f"osascript -e '{tellAapplication}'")
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -41,7 +44,7 @@ def connect_mqtt() -> mqtt_client:
             print(f'Failed to connect, return code {rc}\n')
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
+    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
@@ -49,14 +52,15 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         topic = msg.topic
-        payload = json.loads(msg.payload.decode())
+        payload = msg.payload.decode()
         print(f'Received `{payload}` from `{topic}` topic')
-        if payload['action'] == 'single_left':
-            keyboard.send('k')
-            notify('Left player', title='mqtt2kbrd', soundname='Basso')
-        elif payload['action'] == 'single_right':
-            keyboard.send('k')
-            notify('Right player', title='mqtt2kbrd', soundname='Blow')
+
+        if topic == 'zigbee2mqtt/Button1/action':
+            press('a')
+        elif topic == 'zigbee2mqtt/Button2/action':
+            press('b')
+        elif topic == 'zigbee2mqtt/Button3/action':
+            press('c')
 
     client.subscribe(topics)
     client.on_message = on_message
